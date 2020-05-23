d2c1c1db1d9c776c01018ef800ca3e04d65b9bb1
Dóra Varnyú <varnyu.dora@gmail.com>
2020-05-23 06:57:32 +0200

    fixed faulty build

---
+++ build/precache-manifest.28f47d9dd994d08dbf2e89f31f2380cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2104f8010ef0a6fd54dcfb60283b6f3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e83fb981d8ba2db18093",
+    "url": "./static/css/main.f0365dd7.chunk.css"
+  },
+  {
+    "revision": "5ed28e986c9d3ccfaaa3",
+    "url": "./static/js/2.0f4066fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0f4066fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e83fb981d8ba2db18093",
+    "url": "./static/js/main.47dec9a8.chunk.js"
+  },
+  {
+    "revision": "3b5176513c78f0fdd135",
+    "url": "./static/js/runtime-main.03defe92.js"
+  }
+]);
